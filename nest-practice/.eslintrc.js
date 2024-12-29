24b88595cdc393fcbe1a1cb87702b94c74e928b1
ghghwldk <ghghwldk@gmail.com>
2024-12-29 13:31:41 +0900

    nest-practice eslint 설정, intellij module 설정

--- nest-practice/.eslintrc.js
+++ nest-practice/.eslintrc.js
@@ -17,9 +17,18 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+
+    "indent": "off", // 탭으로 분리(ide에서 4로 설정할 것), 데코레이터 이후의 노드는 무시
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        useTabs: false,
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
